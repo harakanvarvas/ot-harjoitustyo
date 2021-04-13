8ec1caa1050bac2d8a91b57fdf8cbede20cd9008
harakanvarvas <niksu.hynninen@helsinki.fi>
2021-04-13 19:55:33 +0300

    Lisätty testejä

--- harjoitustyo/tasks.py
+++ harjoitustyo/tasks.py
@@ -7,8 +7,14 @@ def test(ctx):
 @task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src")
+@task
+def coverage_percent(ctx):
+    ctx.run("coverage report -m")
 
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html")
 
+@task
+def start(ctx):
+    ctx.run("")
